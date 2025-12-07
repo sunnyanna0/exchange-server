701b51b2d10a1785c58e27002fd670600e8ca348
sunny <anna7789@naver.com>
2025-12-08 00:07:30 +0900

    fix: GraphQL 빌드 오류로 @key directive 제거

--- src/schema.js
+++ src/schema.js
@@ -2,50 +2,50 @@ import { gql } from 'graphql-tag';
 
 export const typeDefs = gql`
   type Query {
-  "환율조회"
-  getExchangeRate(src:String!, tgt:String!): ExchangeInfo
-}
+    "환율조회"
+    getExchangeRate(src: String!, tgt: String!): ExchangeInfo
+  }
 
-type Mutation {
-  "환율등록, src, tgt, date에 대해서 upsert"
-  postExchangeRate(info: InputUpdateExchangeInfo): ExchangeInfo
-  "환율삭제, 해당일자의 해당 통화간 환율을 삭제"
-  deleteExchangeRate(info: InputDeleteExchangeInfo): ExchangeInfo
-}
+  type Mutation {
+    "환율등록, src, tgt, date에 대해서 upsert"
+    postExchangeRate(info: InputUpdateExchangeInfo!): ExchangeInfo
+    "환율삭제, 해당일자의 해당 통화간 환율을 삭제"
+    deleteExchangeRate(info: InputDeleteExchangeInfo!): ExchangeInfo
+  }
 
-"환율업데이트정보 Input"
-input InputUpdateExchangeInfo {
-  "소스통화, krw, usd"
-  src: String!
-  "타겟통화"
-  tgt: String!
-  "환율"
-  rate: Float!
-  "기준일, 값이 없으면, 최신일자로 등록"
-  date: String
-}
+  "환율업데이트정보 Input"
+  input InputUpdateExchangeInfo {
+    "소스통화, krw, usd"
+    src: String!
+    "타겟통화"
+    tgt: String!
+    "환율"
+    rate: Float!
+    "기준일, 값이 없으면, 최신일자로 등록"
+    date: String
+  }
 
-"환율삭제 Input"
-input InputDeleteExchangeInfo {
-  "소스통화"
-  src: String!
-  "타겟통화"
-  tgt: String!
-  "기준일"
-  date: String!
-}
+  "환율삭제 Input"
+  input InputDeleteExchangeInfo {
+    "소스통화"
+    src: String!
+    "타겟통화"
+    tgt: String!
+    "기준일"
+    date: String!
+  }
 
-"환율정보"
-type ExchangeInfo @key(fields: "src, tgt") {
-  "소스통화"
-  src: String!
-  "타겟통화"
-  tgt: String!
-  "환율"
-  rate: Float!
-  "기준일, 값이 없으면, 최신일자의 환율을 응답"
-  date: String!
-}
+  "환율정보"
+  type ExchangeInfo {
+    "소스통화"
+    src: String!
+    "타겟통화"
+    tgt: String!
+    "환율"
+    rate: Float!
+    "기준일, 값이 없으면, 최신일자의 환율을 응답"
+    date: String!
+  }
 `;
 
 export default typeDefs;
